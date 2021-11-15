4245485d78cfb89123f0e4f8561174031472b950
Anshul Nigam <74321084+Anshul-13J@users.noreply.github.com>
2021-11-16 00:03:14 +0530

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,15 +1,15 @@
 #include <LiquidCrystal.h>
-
-const int rs=11, en=10, d4=2, d5=3, d6=4, d7=5;  //LCD pins
-const int pirPin=13, pr=A0, relay=12 ;			 //other pins
-int prValue,pir,motion=0,flag=0;						 //values
+int count=0;
+const int rs=12, en=11, d4=2, d5=3, d6=4, d7=5;    //LCD pins
+const int pirPin=8, pr=A0, relay=13 ;              //other pins
+int prValue,pir,motion=0,flag=0;                   //values
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int calculateprValue() 							 //Function to calculate
-{												 //Photoresistor value
+int calculateprValue()               //Function to calculate
+{                                    //Photoresistor value
   int value= analogRead(pr);
-  value=map(value,1,310,0,100);
-  return value; 								 //Value of PR
+  value=map(value,10,700,0,100);
+  return value;                  //Value of PR
 }
 
 void setup()
@@ -17,19 +17,19 @@ void setup()
   pinMode(pr, INPUT);
   pinMode(relay,OUTPUT);
   pinMode(pirPin,INPUT);
-  digitalWrite(relay,LOW);						 //Switching off relay module if on
+  digitalWrite(relay,LOW);             //Switching off relay module if on
   lcd.begin(16,2);
   lcd.setCursor(0,0);
   lcd.print("WELCOME");
   delay(2000);
   lcd.clear();
   lcd.home();
-  lcd.print("Calibrating PIR");					 //PIR sensor needs caliberating
+  lcd.print("Calibrating PIR");          //PIR sensor needs calibrating
   lcd.setCursor(0,1);
   for(int i=0;i<16;i++)
   {
     lcd.print("*");
-  	delay(150);
+    delay(3000);
   }
   lcd.clear();
   lcd.setCursor(3,0);
@@ -43,22 +43,26 @@ void setup()
 }
 void loop()
 {
-  if(prValue==100)								 //Checking if previous value of
-  {												 //PR is 100, then we need to clear	
-    lcd.setCursor(14,0);						 //3rd digit of value on display
-    lcd.print(" ");								 //to observe proper output on lcd
+  if(prValue==100)                     //Checking if previous value of
+  {                                    //PR is 100, then we need to clear 
+    lcd.setCursor(14,0);               //3rd digit of value on display
+    lcd.print(" ");                    //to observe proper output on lcd
   }
   
   prValue= calculateprValue();
   pir= digitalRead(pirPin);
-  if(pir==HIGH && prValue<20)					 //Condition to detect motion as 
-  {												 //well as low brightness in the room
+  if(pir==HIGH && prValue<6)          //Condition to detect motion as 
+  {                                   //well as low brightness in the room
     digitalWrite(relay,HIGH);
     if(flag!=1)
-    	motion++;
+      motion++;
     flag=1;
+    lcd.setCursor(12,0);
+    lcd.print(prValue);
+    
+    lcd.print("  ");
     lcd.setCursor(12,1);
-  	lcd.print(motion);
+    lcd.print(motion);
     delay(5000);
   }
   else{
@@ -67,7 +71,17 @@ void loop()
   }
   lcd.setCursor(12,0);
   lcd.print(prValue);
+  lcd.print("  ");
   lcd.setCursor(12,1);
   lcd.print(motion);
   delay(500);
+  if(count%20==0 && count!=0)
+  {
+    lcd.clear();
+    lcd.home();
+    lcd.print("Brightness: ");
+    lcd.setCursor(0,1);
+    lcd.print("Motion det.:"); 
+  }
+  count++;
 }
